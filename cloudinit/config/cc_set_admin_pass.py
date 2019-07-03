372a82bcdb92ebf9033954d6c27d1cf69d67a373
Pavel Glushchak <pglushchak@virtuozzo.com>
2019-07-03 14:04:23 +0300

    admin_pass: set for admin instead of root
    Signed-off-by: Pavel Glushchak <pglushchak@virtuozzo.com>

--- cloudinit/config/cc_set_admin_pass.py
+++ cloudinit/config/cc_set_admin_pass.py
@@ -4,10 +4,11 @@ from cloudinit import util
 def handle(_name, cfg, cloud, log, args):
     admin_pass = cloud.get_admin_pass()
     if not admin_pass:
-        log.debug("admin_pass is not in datasource, could not set root password")
-    else:
-        log.debug("Changing password for root with admin_pass")
-        try:
-            util.subp(['chpasswd'], "root:%s\n" % admin_pass)
-        except util.ProcessExecutionError as e:
-            util.logexc(log, "chpasswd failed with exit code %d" % e.exit_code)
+        log.debug("admin_pass is not in datasource, could not set admin password")
+        return
+
+    log.warning("Changing password for admin with admin_pass")
+    try:
+        util.subp(['chpasswd'], "admin:%s\n" % admin_pass)
+    except util.ProcessExecutionError as e:
+        util.logexc(log, "chpasswd failed with exit code %d" % e.exit_code)
